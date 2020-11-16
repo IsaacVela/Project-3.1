0f096e42500c567a8ab3d51d2da07000c0dc6df6
IsaacVela <isaacvela@gmail.com>
2020-11-15 18:33:16 -0600

    Initial Commit

---
+++ Project 3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 3.1
+//
+//  Created by Student on 11/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 3.1/Project_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3_1App.swift
+//  Project 3.1
+//
+//  Created by Student on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct Project_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
